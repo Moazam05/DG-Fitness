496e73a4f4176a02010afc6eba11930ffc9527dc
Salman Moazam <salmanmoazam08@gmail.com>
2021-03-06 15:08:04 +0500

    Hero Css

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -1,24 +1,29 @@
-import React from 'react'
+import React from 'react';
+import './Hero.css';
 
-function Hero(comingImage="") {
-    return  <>
-        <div className='banner_area d-flex align-items-center' style={{ backgroundImage: `url('img/banner.jpg')` }} >
-          <div className='container'>
-            <div className='row'>
-              <div className='col-12 text-center'>
-                <h1>
-                  Build <span>Your</span> Body
-                </h1>
-                <a href='/' className='button'>
-                  {/* <a href='#' className='button'> */}
-                  START NOW!
-                </a>
-              </div>
+function Hero(comingImage = '') {
+  return (
+    <>
+      <div
+        className='banner_area d-flex align-items-center'
+        style={{ backgroundImage: `url('img/banner.jpg')` }}
+      >
+        <div className='container'>
+          <div className='row'>
+            <div className='col-12 text-center'>
+              <h1>
+                Build <span>Your</span> Body
+              </h1>
+              <a href='/' className='button'>
+                {/* <a href='#' className='button'> */}
+                START NOW!
+              </a>
             </div>
           </div>
         </div>
-        </>
-
+      </div>
+    </>
+  );
 }
 
-export default Hero
+export default Hero;
